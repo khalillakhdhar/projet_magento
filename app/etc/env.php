3c2da3a03e8f3c91577ca6d649622189ba8fb531
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-19 19:56:38 +0100

    Add files via upload

---
+++ app/etc/env.php
@@ -0,0 +1,73 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'e937515b548d8f46b98e1d884ad962b5'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'pfejrijni',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1',
+                'driver_options' => [
+
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '845_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '845_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Thu, 19 Nov 2020 17:56:42 +0000'
+    ]
+];
